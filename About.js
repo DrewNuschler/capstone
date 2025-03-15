56346387d88513a29cd214e7a9e5e74c4e8c45b3
DrewNuschler <lookup1929@gmail.com>
2025-03-14 23:02:47 -0500

    Update

--- About.js
+++ About.js
@@ -1,24 +1,19 @@
-
 let part1 = document.getElementById("part1");
 let messageParagraph = document.getElementById("messageParagraph");
 
 function click1Part1() {
-    let hey;
-    hey = {};
-    hey.you = prompt("What's your name?");
-  
-    messageParagraph.innerHTML = `${hey.you}, Thank you for visiting my new portfolio page.`;
-  
-   }
+  let hey;
+  hey = {};
+  hey.you = prompt("What's your name?");
 
-  part1.onclick = click1Part1;
+  messageParagraph.innerHTML = `${hey.you}, Thank you for visiting my new portfolio page.`;
+}
 
-  function show() {
-    /* Access image by id and change 
+part1.onclick = click1Part1;
+
+function show() {
+  /* Access image by id and change 
     the display property to block*/
-    document.getElementById('image')
-        .style.display = "block";
-    document.getElementById('btnID')
-        .style.display = "none";
-      } 
-
+  document.getElementById("image").style.display = "block";
+  document.getElementById("btnID").style.display = "none";
+}
